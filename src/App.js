c17f84308dd09d8c0cc0da1ace5400482d00946c
pooriavakili <pooriavakili09@gmail.com>
2020-01-25 23:58:53 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import React,{Component} from 'react';
+import React, {useState} from 'react';
 import Toolbar from "./components/Toolbar/Toolbar";
 import './index.css'
 import SideDrawer from "./components/SideDrawer/SideDrawer";
 import BackDrop from "./components/BackDrop/BackDrop";
-export default class App extends Component {
-    state={
-      sideDrawerOpen:false
+export default function App() {
+    const [drawer, setDrawer] = useState({
+        sideDrawerOpen: false
+    })
+    const drawerToggleClickHandler = () => {
+        setDrawer({
+            sideDrawerOpen: !drawer.sideDrawerOpen
+        })
     };
-    drawerToggleClickHandler=()=>{
-this.setState((prevState)=>{
-    return{sideDrawerOpen: !prevState.sideDrawerOpen}
-})
-    };
-    backDropClickHandler=()=>{
-        this.setState({
-            sideDrawerOpen:false
+    const backDropClickHandler = () => {
+        setDrawer({
+            sideDrawerOpen: false
+
         })
     }
-  render() {
-      let backdrop;
-      if (this.state.sideDrawerOpen){
-          backdrop=<BackDrop click={this.backDropClickHandler}/>
-      }
+    let backdrop;
+    if (this.state.sideDrawerOpen){
+        backdrop=<BackDrop click={backDropClickHandler}/>
+    }
     return (
         <div className='App'>
-<Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
-           <SideDrawer show={this.state.sideDrawerOpen}/>
-            {backdrop }
-<main className='main'>
-    <p>this is page content</p>
-</main>
+            <Toolbar drawerClickHandler={drawerToggleClickHandler}/>
+            <SideDrawer show={this.state.sideDrawerOpen}/>
+            {backdrop}
+            <main className='main'>
+                <p>this is page content</p>
+            </main>
         </div>
-    );
-  }
+    )
+
+
 }
